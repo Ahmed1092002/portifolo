5c15eee25dc86f105f118490cf0686ff1056a7db
Ahmed Tamer Elsayed <112315071+Ahmed1092002@users.noreply.github.com>
2025-11-17 14:55:35 +0200

    feat: Add Head component for improved SEO and metadata management

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import dynamic from "next/dynamic";
+import Head from "next/head";
 import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -29,21 +30,52 @@ const FloatingObjects = dynamic(() => import("./components/FloatingObjects"), {
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-(--background) relative overflow-hidden">
-      <ParticleBackground />
-      <FloatingObjects />
-      <FloatingShapes />
-      <div className="relative z-10">
-        <Navigation />
-        <Hero />
-        <About />
-        <Education />
-        <Experience />
-        <Skills />
-        <Projects />
-        <Contact />
-        <Footer />
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>
+          Ahmed Tamer — Software Engineer &amp; Flutter &amp; Web Developer
+        </title>
+        <meta
+          name="description"
+          content="A Software Engineer experienced in Flutter and React ecosystems, with a solid background in building mobile and web applications. I've worked on ERP systems, AI chat applications, invoicing systems, and payment solutions at AppLogica."
+        />
+        <meta
+          name="keywords"
+          content="Ahmed Tamer, Software Engineer, Flutter Developer, Web Developer"
+        />
+        <meta
+          property="og:title"
+          content="Ahmed Tamer — a Software Engineer &amp; Flutter &amp; Web Developer"
+        />
+        <meta
+          property="og:description"
+          content="A Software Engineer experienced in Flutter and React ecosystems, with a solid background in building mobile and web applications."
+        />
+        <meta
+          property="og:image"
+          content="/Gemini_Generated_Image_pc7nb0pc7nb0pc7n.ico"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ahmed-tamer.vercel.app" />
+
+
+      </Head>
+      <main className="min-h-screen bg-(--background) relative overflow-hidden">
+        <ParticleBackground />
+        <FloatingObjects />
+        <FloatingShapes />
+        <div className="relative z-10">
+          <Navigation />
+          <Hero />
+          <About />
+          <Education />
+          <Experience />
+          <Skills />
+          <Projects />
+          <Contact />
+          <Footer />
+        </div>
+      </main>
+    </>
   );
 }
