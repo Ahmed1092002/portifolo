7217e47971685fd3720c5987af38df1dad210443
Ahmed Tamer Elsayed <112315071+Ahmed1092002@users.noreply.github.com>
2026-07-31 18:05:22 +0300

    feat: update page metadata and replace deprecated image files

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ const ParticleBackground = dynamic(
   {
     ssr: false,
     loading: () => null,
-  }
+  },
 );
 const FloatingObjects = dynamic(() => import("./components/FloatingObjects"), {
   ssr: false,
@@ -33,32 +33,27 @@ export default function Home() {
     <>
       <Head>
         <title>
-          Ahmed Tamer — Software Engineer &amp; Flutter &amp; Web Developer
+          Ahmed Tamer — Frontend Developer &amp; Flutter &amp; Web Developer
         </title>
         <meta
           name="description"
-          content="A Software Engineer experienced in Flutter and React ecosystems, with a solid background in building mobile and web applications. I've worked on ERP systems, AI chat applications, invoicing systems, and payment solutions at AppLogica."
+          content="A Frontend Developer experienced in Flutter and React ecosystems, with a solid background in building mobile and web applications. I've worked on ERP systems, AI chat applications, invoicing systems, and payment solutions at AppLogica."
         />
         <meta
           name="keywords"
-          content="Ahmed Tamer, Software Engineer, Flutter Developer, Web Developer"
+          content="Ahmed Tamer, Frontend Developer, Flutter Developer, Web Developer"
         />
         <meta
           property="og:title"
-          content="Ahmed Tamer — a Software Engineer &amp; Flutter &amp; Web Developer"
+          content="Ahmed Tamer — a Frontend Developer &amp; Flutter &amp; Web Developer"
         />
         <meta
           property="og:description"
-          content="A Software Engineer experienced in Flutter and React ecosystems, with a solid background in building mobile and web applications."
+          content="A Frontend Developer experienced in Flutter and React ecosystems, with a solid background in building mobile and web applications."
         />
-        <meta
-          property="og:image"
-          content="/Gemini_Generated_Image_pc7nb0pc7nb0pc7n.ico"
-        />
+        <meta property="og:image" content="/image ico.ico" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://ahmed-tamer.vercel.app" />
-
-
       </Head>
       <main className="min-h-screen bg-(--background) relative overflow-hidden">
         <ParticleBackground />
